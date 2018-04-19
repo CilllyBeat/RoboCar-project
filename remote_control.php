855ff5ff0df43bf3eb856e2cd89e45f81e7d522f
CilllyBeat <37693688+CilllyBeat@users.noreply.github.com>
2018-04-19 12:21:33 +0200

    computer
    With variables inserted in php

--- remote_control.php
+++ remote_control.php
@@ -27,56 +27,68 @@
 		</form>
 		
 		<?php
+			//creating vairables for easy changes
+			$forward = 27;
+			$fast_forward = 17;
+			$turn_left = 26;
+			$turn_right = 18;
+			$strafe_right = 6;
+			$strafe_left = 5;
+			$reverse = 24;
+			$reverse_right = 20;
+			$reverse_left = 16;
+			
 			//Setting up GPIO outputs
-			//consider making variables for GPIOs for easy changes
-			$setmode27 = shell_exec ("/usr/local/bin/gpio -g mode 27 out" );
-			$setmode17 = shell_exec ("/usr/local/bin/gpio -g mode 17 out" );
-			$setmode26 = shell_exec ("/usr/local/bin/gpio -g mode 26 out" );
-			$setmode18 = shell_exec ("/usr/local/bin/gpio -g mode 18 out" );
-			$setmode6 = shell_exec ("/usr/local/bin/gpio -g mode 6 out" );
-			$setmode5 = shell_exec ("/usr/local/bin/gpio -g mode 5 out" );
-			$setmode24 = shell_exec ("/usr/local/bin/gpio -g mode 24 out" );
-			$setmode20 = shell_exec ("/usr/local/bin/gpio -g mode 20 out" );
-			$setmode16 = shell_exec ("/usr/local/bin/gpio -g mode 16 out" );
+			$setmode27 = shell_exec ("/usr/local/bin/gpio -g mode $forward out" );
+			$setmode17 = shell_exec ("/usr/local/bin/gpio -g mode $fast_forward out" );
+			$setmode26 = shell_exec ("/usr/local/bin/gpio -g mode $turn_left out" );
+			$setmode18 = shell_exec ("/usr/local/bin/gpio -g mode $turn_right out" );
+			$setmode6 = shell_exec ("/usr/local/bin/gpio -g mode $strafe_right out" );
+			$setmode5 = shell_exec ("/usr/local/bin/gpio -g mode $strafe_left out" );
+			$setmode24 = shell_exec ("/usr/local/bin/gpio -g mode $reverse out" );
+			$setmode20 = shell_exec ("/usr/local/bin/gpio -g mode $reverse_right out" );
+			$setmode16 = shell_exec ("/usr/local/bin/gpio -g mode $reverse_left out" );
+			
+
 			//sets buttons to 1
 			if(isset($_POST[fwd])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 27 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $forward 1" );
 			}
 			elseif(isset($_POST[fsfwd])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 17 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $fast_forward 1" );
 			}
 			elseif(isset($_POST[left])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 26 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $turn_left 1" );
 			}
 			elseif(isset($_POST[right])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 18 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $turn_right 1" );
 			}
 			elseif(isset($_POST[Sright])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 6 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $strafe_right 1" );
 			}
 			elseif(isset($_POST[Sleft])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 5 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $strafe_left 1" );
 			}
 			elseif(isset($_POST[rvs])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 24 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $reverse 1" );
 			}
 			elseif(isset($_POST[Rright])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 20 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $reverse_right 1" );
 			}
 			elseif(isset($_POST[Rleft])){
-				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write 16 1" );
+				$gpio_on = shell_exec ("/usr/local/bin/gpio -g write $reverse_left 1" );
 			}
 			//clears GPIOs
 			elseif(isset($_POST[off])){
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 27 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 17 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 26 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 18 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 6 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 5 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 24 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 20 0" );
-				$gpio_off = shell_exec("usr/local/bin/gpio -g write 16 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $forward 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $fast_forward 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $turn_left 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $turn_right 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $strafe_right 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $strafe_left 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $reverse 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $reverse_right 0" );
+				$gpio_off = shell_exec("usr/local/bin/gpio -g write $reverse_left 0" );
 			}
 		?>
 	</main>
@@ -86,5 +98,5 @@
 	*look in to javascript buttons
 	*tinker with location of buttons 
 	*tinker with layout
-	*make variables or GPIO values for easy changes
+	*make variables or GPIO values for easy changes [DONE]
 -->
